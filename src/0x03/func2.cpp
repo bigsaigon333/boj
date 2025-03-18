ad8b91fff2f68dae6b5bfffc86a6910149bcd7eb
bigsaigon333 <likepeppermint@gmail.com>
2025-03-18 20:34:31 +0900

    feat: 3강 배열 강의의 func2를 푼다

---
+++ src/0x03/func2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func2(int arr[], int N)
+{
+  int count[105];
+  fill(count, count + 105, 0);
+
+  for (int i = 0; i < N; i++)
+  {
+    if (arr[100 - arr[i]] > 0)
+    {
+      return 1;
+    }
+    arr[i]++;
+  }
+
+  return 0;
+}
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int arr[3] = {1, 52, 48};
+
+  cout << func2(arr, 3) << '\n';
+}
